81bd1399674b6ac77c457013f01adb1c6b998bc0
bunny-ml <abhishek260204@gmail.com>
2025-08-13 13:23:21 +0530

    final chnage

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from app_backend.api.routes import Flask_app
 if __name__ == "__main__":
     app_instance = Flask_app()
     app = app_instance.get_app()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
